f0b080208a7003873e5a87c70e2ff9591d439e3c
Afonso Rosa <afonsomartinelirosa@gmail.com>
2020-04-08 22:31:13 -0300

    Add fixing the language

--- docker-install.sh
+++ docker-install.sh
@@ -4,3 +4,4 @@ sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get autoremove -y
 sudo apt-get install curl wget vim -y
 sudo curl -fsSL https://get.docker.com | bash
 sudo systemctl enable docker
+sudo locale-gen pt_BR.UTF-8
